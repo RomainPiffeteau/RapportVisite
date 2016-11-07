698625d28dcf222a084cf3a553852ee54b4a07b8
etienne <et.bourreau@laposte.net>
2016-11-07 21:54:34 +0100

    all models made

--- app/src/main/java/sio2/rapportvisite/model/Visitor.java
+++ app/src/main/java/sio2/rapportvisite/model/Visitor.java
@@ -1,7 +1,34 @@
 package sio2.rapportvisite.model;
 
-/**
- * Created by bourreau on 07/11/2016.
- */
+import java.util.ArrayList;
+
 public class Visitor {
+    private int id;
+    private String name;
+    private String firstName;
+
+    public static ArrayList<Visitor> allVisitors = new ArrayList<>();
+
+    public Visitor(int id, String name, String firstName){
+        this.id = id;
+        this.name = name;
+        this.firstName = firstName;
+        allVisitors.add(this);
+    }
+
+    public String getNameFromId(int id){
+        for(Visitor vis : allVisitors){
+            if(vis.id==id)
+                return vis.name;
+        }
+        return "NameNoSuchId";
+    }
+
+    public String getFirstNameFromId(int id){
+        for(Visitor vis : allVisitors){
+            if(vis.id==id)
+                return vis.firstName;
+        }
+        return "FirstNameNoSuchId";
+    }
 }
